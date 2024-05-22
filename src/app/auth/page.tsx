b2208b942efc111719fc4d7f5f557dd33256c970
gildembergleite <gildembergleite@gmail.com>
2024-05-22 17:57:16 -0300

    feat: created pages and load buttons

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,24 +1,27 @@
-import { LoginButton } from '@/components/auth-buttons/login-button'
+import { LogoutButton } from '@/components/auth-buttons/logout-button'
+import { UsersPageButton } from '@/components/routing-buttons/user-page-button'
 import { authApi } from '@/services/auth-api'
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
-export default async function Home() {
+
+export default async function AuthPage() {
   const cookieStore = cookies()
   const token = cookieStore.get('@refresh-token')
   await authApi.refreshToken(token?.value).then((res) => {
-    if (res) {
-      redirect('/auth')
+    if (!res) {
+      redirect('/')
     }
   })
 
   return (
     <main className="flex flex-col w-full min-h-screen justify-center items-center bg-zinc-900 gap-6">
       <div className="text-white text-center space-y-2">
-        <h1 className="text-4xl">Hello, world!</h1>
+        <h1 className="text-4xl">Auth Page</h1>
         <p className="text-xl">ApiService Example</p>
       </div>
       <div className="flex flex-col gap-2">
-        <LoginButton />
+        <UsersPageButton />
+        <LogoutButton />
       </div>
     </main>
   )
--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,24 +1,31 @@
-import { LoginButton } from '@/components/auth-buttons/login-button'
+import { AuthPageButton } from '@/components/routing-buttons/auth-page-button'
+import { CreateUserButton } from '@/components/users-buttons/create-user-button'
+import { GetUsersButton } from '@/components/users-buttons/get-users-button'
+import { UpdateUserButton } from '@/components/users-buttons/updated-user-button'
 import { authApi } from '@/services/auth-api'
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
-export default async function Home() {
+
+export default async function UsersPage() {
   const cookieStore = cookies()
   const token = cookieStore.get('@refresh-token')
   await authApi.refreshToken(token?.value).then((res) => {
-    if (res) {
-      redirect('/auth')
+    if (!res) {
+      redirect('/')
     }
   })
 
   return (
     <main className="flex flex-col w-full min-h-screen justify-center items-center bg-zinc-900 gap-6">
       <div className="text-white text-center space-y-2">
-        <h1 className="text-4xl">Hello, world!</h1>
+        <h1 className="text-4xl">Users Page</h1>
         <p className="text-xl">ApiService Example</p>
       </div>
       <div className="flex flex-col gap-2">
-        <LoginButton />
+        <AuthPageButton />
+        <UpdateUserButton userId={authApi.userData?.id} />
+        <GetUsersButton />
+        <CreateUserButton />
       </div>
     </main>
   )
